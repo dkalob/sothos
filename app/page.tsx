5e06a03637bff6486438357dbdbd90562098b0d0
Douglas <kaloblian@gmail.com>
2026-07-14 20:55:02 -0300

    adiciona shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@base-ui/react';
+import { Plus } from 'lucide-react';
+import { Card } from '@/components/ui/card';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="h-screen flex justify-center items-center gap-4">
+      <Card className="w-lg p-4 bg-gray-100">
+        <Input placeholder="Digite algo..."/>
+        <Separator className="my-2"/>
+        <Button><Plus/>Cadastrar</Button>
+      </Card>
+    </div>
   );
 };
 
---
+++ instrucoes.md
@@ -0,0 +1,11 @@
+## PROJETO INICIADO COM NEXT.JS
+npx create-app-next@latest
+rodar com npm run dev
+
+## SHADCN
+https://ui.shadcn.com/docs/components
+Shadcn : pacote com componentes prontos
+Para instalar: npx shadcn@latest init
+Para usar componentes: npx shadcn@latest add (nome)
+Todos os componentes são criadaos na pasta components > ui 
+Eles usam a biblioteca de ícones do Lucide
